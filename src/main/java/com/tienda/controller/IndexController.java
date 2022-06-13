b37c3f79c39c3ba62f89c36f618d0a2561ca4630
El macho <El macho@DESKTOP-DA1SJUC>
2022-06-12 20:16:00 -0600

    Commit de semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        log.info("Estamos usando una arquitectura mvc");
+        
+        var clientes = clienteDao.findAll();
+        model.addAttribute("clientes",clientes);
+        return "index";
+    }
+    
+}
