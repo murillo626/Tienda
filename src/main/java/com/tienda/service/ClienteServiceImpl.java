f89a9dff759b52f0195a57b40e14ba9d7cba8bd4
El macho <El macho@DESKTOP-DA1SJUC>
2022-07-13 17:55:18 -0600

    Materia semana 08 y Tarea crud

--- src/main/java/com/tienda/service/ClienteServiceImpl.java
+++ src/main/java/com/tienda/service/ClienteServiceImpl.java
@@ -1,7 +1,9 @@
 package com.tienda.service;
 
 import com.tienda.dao.ClienteDao;
+import com.tienda.dao.CreditoDao;
 import com.tienda.domain.Cliente;
+import com.tienda.domain.Credito;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ public class ClienteServiceImpl implements ClienteService {
     
     @Autowired
     private ClienteDao clienteDao;
+    @Autowired
+    private CreditoDao creditoDao;
 
     @Override
     @Transactional(readOnly = true)
@@ -22,6 +26,9 @@ public class ClienteServiceImpl implements ClienteService {
     @Override
     @Transactional
     public void save(Cliente cliente) {
+        Credito credito = cliente.getCredito();
+        credito=creditoDao.save(credito);
+        cliente.setCredito(credito);
         clienteDao.save(cliente);
     }
 
